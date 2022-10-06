0045e4d68415d20ba2901c28bce8f015e43675cd
Hugo STAEDELIN <hugostaedelin68@gmail.com>
2022-10-06 09:27:09 +0200

    Initial commit

---
+++ src/test/java/fr/hstaedelin/neograph/NeographApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hstaedelin.neograph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeographApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
