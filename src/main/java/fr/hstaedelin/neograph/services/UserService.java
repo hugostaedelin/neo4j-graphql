1825f61433b73252e2eab26b39b0feea415e9086
Hugo STAEDELIN <hugostaedelin68@gmail.com>
2022-10-12 11:18:17 +0200

    feat: add find by id method

--- src/main/java/fr/hstaedelin/neograph/services/UserService.java
+++ src/main/java/fr/hstaedelin/neograph/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +16,9 @@ public class UserService {
 
     public Collection<User> getAll() {
         return userRepository.getAllUsers();
+    }
+
+    public Optional<User> getById(Long id) {
+        return userRepository.findById(id);
     }
 }
